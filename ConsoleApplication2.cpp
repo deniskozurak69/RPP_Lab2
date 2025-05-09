b3b9f99dc0b5886db4933b1b03810e8db7b09434
deniskozurak69 <161163382+deniskozurak69@users.noreply.github.com>
2025-05-10 02:01:40 +0300

    Update ConsoleApplication2.cpp

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -2,18 +2,54 @@
 #include <vector>
 #include <omp.h>
 #include <climits>
+#include <fstream>
 using namespace std;
 const int INF = 1e9;
-void initializeGraph(std::vector<std::vector<int>>& dist, int N) 
+vector<vector<int>> generateConnectedGraph(int N, int minWeight, int maxWeight)
 {
-    for (int i = 0; i < N; ++i)
+    const int INF = 1e9;
+    vector<vector<int>> adjMatrix(N, vector<int>(N, INF));
+    srand(time(0));
+    for (int i = 0; i < N; ++i) adjMatrix[i][i] = 0;
+    for (int i = 1; i < N; ++i)
     {
-        for (int j = 0; j < N; ++j)
+        int weight = minWeight + rand() % (maxWeight - minWeight + 1);
+        adjMatrix[i][i - 1] = weight;
+        adjMatrix[i - 1][i] = weight;
+    }
+    int extraEdges = rand() % (N * (N - 1) / 2 - (N - 1));
+    for (int i = 1; i <= extraEdges; ++i)
+    {
+        int u = rand() % N;
+        int v = rand() % N;
+        if ((u != v) && (abs(u - v) != 1) && (adjMatrix[u][v] == INF))
         {
-            if (i == j) dist[i][j] = 0;
-            else dist[i][j] = rand() % 100 + 1;
+            int weight = minWeight + rand() % (maxWeight - minWeight + 1);
+            adjMatrix[u][v] = adjMatrix[v][u] = weight;
         }
     }
+    return adjMatrix;
+}
+void adjMatrixToFile(const vector<vector<int>>& matrix, const string& filename)
+{
+    int N = matrix.size();
+    ofstream fout(filename);
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j) fout << matrix[i][j] << " ";
+        fout << "\n";
+    }
+    fout.close();
+}
+void answersToFile(const vector<vector<int>>& matrix, const string& filename)
+{
+    int N = matrix.size();
+    ofstream fout(filename);
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = i + 1; j < N; ++j) fout << "The shortest distance between " << i << " and " << j << " is: " << matrix[i][j] << "\n";
+    }
+    fout.close();
 }
 void floydWarshall(std::vector<std::vector<int>>& dist, int N, int num_threads) 
 {
@@ -38,14 +74,17 @@ int main()
     cin >> N;
     cout << "Enter the number of threads: ";
     cin >> num_threads;
-    vector<vector<int>> dist(N, vector<int>(N));
-    initializeGraph(dist, N);
+    vector<vector<int>> Matrix;
+    Matrix = generateConnectedGraph(N, 1, 10);
+    adjMatrixToFile(Matrix, "adjacency_matrix.txt");
     double start_time = omp_get_wtime();
-    floydWarshall(dist, N, num_threads);
+    floydWarshall(Matrix, N, num_threads);
     double end_time = omp_get_wtime();
+    answersToFile(Matrix, "shortest_paths.txt");
     std::cout << "Execution time: " << (end_time - start_time) << " seconds" << "\n";
     return 0;
 }
+
 
 
 
